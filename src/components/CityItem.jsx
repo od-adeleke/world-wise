5e76f547bec84a89e6903bc4389515df535131b5
od-adeleke <damoshey08@gmail.com>
2025-03-08 03:02:04 +0100

    added countries feature

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,8 +1,21 @@
+import styles from './CityItem.module.css'
+
+const formatDate = (date) => new Intl.DateTimeFormat('en', {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+  weekday: 'long'
+}).format(new Date(date))
+
 const CityItem = ({city}) => {
-  console.log(city)
+  const {cityName, emoji, date}= city
+
   return (
-    <li>
-      {city}
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+      <button className={styles.deleteBtn}>&times;</button>
     </li>
   )
 }
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,14 +3,14 @@ import CityItem from "./CityItem";
 import styles from './CityList.module.css'
 
 const CityList = ({cities, isLoading}) => {
-  console.log(cities)
   if(isLoading) return <Spinner />
   return (
     <ul className={styles.cityList}>
       {cities.map(city => <CityItem city={city} key={city.id} />
       )}
+      {/* {cities.map(city=> <li key={city.id}>{city.cityName}</li>)} */}
     </ul>
   )
-}
+} 
 
 export default CityList
