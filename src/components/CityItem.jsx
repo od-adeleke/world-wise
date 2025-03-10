f4b529a96b3f70581ebbe39aadd6e4534f1be0be
od-adeleke <damoshey08@gmail.com>
2025-03-10 11:21:13 +0100

    feature: added state management using dynamic URL parameters

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -9,11 +9,11 @@ const formatDate = (date) => new Intl.DateTimeFormat('en', {
 }).format(new Date(date))
 
 const CityItem = ({city}) => {
-  const {cityName, emoji, date, id}= city
+  const {cityName, emoji, date, id, position}= city
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&long=${position.lng}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
