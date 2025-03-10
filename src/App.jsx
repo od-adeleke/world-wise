9978acaa5a28caca4bec334eddd4fb3bc9658536
od-adeleke <damoshey08@gmail.com>
2025-03-10 10:04:38 +0100

    feature: added countries

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AppLayout from './pages/AppLayout'
 import Login from './pages/Login'
 import PageNotFound from './pages/PageNotFound'
 import CityList from './components/CityList'
+import CountryList from './components/CountryList'
 
 import './App.css'
 
@@ -44,13 +45,31 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={
+              <CityList 
+                cities={cities} 
+                isLoading={isLoading} 
+              />
+            }
           />
           <Route
             path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={
+              <CityList 
+                cities={cities} 
+                isLoading={isLoading} 
+              />
+            }
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route 
+            path="countries" 
+            element={
+              <CountryList 
+                cities={cities} 
+                isLoading={isLoading} 
+              />
+            } 
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="login" element={<Login />} />
