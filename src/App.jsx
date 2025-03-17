4552ba5981f111803168a90d79b53a534e229308
od-adeleke <damoshey08@gmail.com>
2025-03-17 11:07:37 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     async function fetchCities() {
       try {
         setIsLoading(true)
-        const res = await fetch(`${BASE_URL}/cities`)
+        const res= await fetch(`${BASE_URL}/cities`)
         const data= await res.json()
         setCities(data)
       } catch(e) {
